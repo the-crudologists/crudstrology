8631afd239a1ab6ac14aa6bf43a28a01b4ea4439
colin <colin.williams91@gmail.com>
2023-01-21 17:58:15 -0600

    add tarot and horoscope fakeData, add Tarot and Horoscope models and schemas, succesful insertion to database for tarot

--- server/index.js
+++ server/index.js
@@ -7,14 +7,15 @@ const DIST_DIR = path.resolve(__dirname, '..', 'dist');
 const app = express();
 
 //middleware
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 app.use(express.static(DIST_DIR));
-
 
 const PORT = 8080;
 
 (async () => {
 
-  //await seeder();
+  await seeder();
 
   app.listen(PORT, () => {
     console.log(`listening on port: ${PORT}`)
