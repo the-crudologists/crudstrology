c978813d413515166e3ece0c22b919010955eac4
Miles Ager <miles.ager13@gmail.com>
2023-01-22 16:56:30 -0600

    remove unnecessary code

--- database/index.js
+++ database/index.js
@@ -2,7 +2,7 @@
 const fakeTarot = require('./fakeData/tarot.json');
 const fakeHoro = require('./fakeData/horoscope.json');
 
-console.log('fake Tarot', fakeTarot.cards[0]);
+// console.log('fake Tarot', fakeTarot.cards[0]);
 
 const Sequelize = require('sequelize');
 const sequelize = new Sequelize(
@@ -14,26 +14,13 @@ const sequelize = new Sequelize(
   }
 );
 
-// Exporting User, using this constant
-// we can perform CRUD operations on
-// 'user' table.
-
-// ******
 const User = sequelize.define('user', {
-  // Column-1, user_id is an object with 
-  // properties like type, keys, 
-  // validation of column.
   user_id: {
-      // Sequelize module has INTEGER Data_Type.
       type: Sequelize.INTEGER,
-      // To increment user_id automatically.
       autoIncrement: true,
-      // user_id can not be null.
       allowNull: false,
-      // For uniquely identify user.
       primaryKey: true
   },
-  // Column-2, name
   name: { 
     type: Sequelize.STRING, 
     allowNull: false 
@@ -64,20 +51,17 @@ const Horoscope = sequelize.define('horoscope', {
 });
 
 // <-- might not need to be async -->
-// changed User.sync to sequelize.sync
 const seeder = async () => {
   console.log('the seeder function was invoked');
   await sequelize.sync({ force: true });
   await User.create({ name: 'PtBarnum' })
     .then(() => {console.log('User Model Create Success')})
-    .catch((err) => {console.log('User Model Create Failure', err)});
+    .catch((err) => {console.error('User Model Create Failure', err)});
   await Tarot.create(fakeTarot.cards[0])
     .then(() => {console.log('Tarot Model Create Success')})
-    .catch((err) => {console.log('Tarot Model Create Failure', err)});
+    .catch((err) => {console.error('Tarot Model Create Failure', err)});
   console.log('Database seeded with a test quote table and data');
 }
-// seeder();
-// module.exports = { seeder };
 
 // module.exports.User = User;
 module.exports.sequelize = sequelize;
