65f8b9b02d755e65a5743b4e13a6f3d05db95835
colin <colin.williams91@gmail.com>
2023-01-22 17:43:30 -0600

    add utils dir for helpers, add zodiacConverter function to select sign based on user input birthdate string, start Astrology jsx component w state hooks

--- client/Index.jsx
+++ client/Index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
+import Horoscope from './Astrology.jsx';
 // import './styles.css';
 // import {
 //   createBrowserRouter,
@@ -20,7 +21,7 @@ ReactDOM.createRoot(document.getElementById('app')).render(
   // <React.StrictMode>
   //     <RouterProvider router={router} />
   // <div>
-    <App />
+    <Horoscope />
   // </div>
 
   // </React.StrictMode>
