34d1303feb17a35a25de57cd88d4ea05051d74b7
colin <colin.williams91@gmail.com>
2023-01-22 21:46:48 -0600

    add profile card display div and styling, name updates and renders from state to new card div

--- client/ProfileCreation.jsx
+++ client/ProfileCreation.jsx
@@ -14,19 +14,27 @@ const CreateProfile = () => {
       <h1>Create Your Profile!</h1>
       <div id='create-profile-form-container'>
         <form>
-          <input
-          placeholder='Name...'
-          onChange={(e) => changeName(e)}
-          ></input>
-          <button className='button'>submit</button>
-          <input placeholder='Birthday...'></input>
-          <button className='button'>submit</button>
-          <input placeholder='Location...'></input>
-          <button className='button'>submit</button>
+          <input className='text' placeholder='Name...' onChange={(e) => changeName(e)}></input>
+          <button className='text'>submit</button>
+          <input className='text' placeholder='Birthday...'></input>
+          <button className='text'>submit</button>
+          <input className='text' placeholder='Location...'></input>
+          <button className='text'>submit</button>
         </form>
       </div>
       <div id='profile-card-container'>
-        <span>{name}</span>
+        <div style={{padding: '15px'}}>
+          <span><b className='text'>Name: </b></span>
+          <span className='text' id='profile-card-name'>{name}</span>
+        </div>
+        <p></p>
+        <div style={{padding: '15px'}}>
+          <span> <b className='text'>Birthday: </b></span>
+        </div>
+        <p></p>
+        <div style={{padding: '15px'}}>
+          <span> <b className='text'>Location: </b></span>
+        </div>
       </div>
     </div>
   )
