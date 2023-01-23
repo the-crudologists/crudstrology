3b185adf73b44be471f0d36ce461722c7e237802
colin <colin.williams91@gmail.com>
2023-01-22 21:02:50 -0600

    profile creation component now has a form with inputs and buttons, as well as 2 container divs, it has state using hooks and a function which handles change on user input event updating the name state using its setter function correctly

--- client/ProfileCreation.jsx
+++ client/ProfileCreation.jsx
@@ -1,18 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 
 const CreateProfile = () => {
+
+  const [name, setName] = useState('Jean Valgene');
+
+  const changeName = (e) => {
+    setName(prevName => (prevName = e.target.value));
+    console.log(name);
+  }
+
   return (
     <div className='create-profile'>
       <h1>Create Your Profile!</h1>
       <div id='create-profile-form-container'>
         <form>
-          <input placeholder='Name...'></input>
-          <button>submit</button>
+          <input
+          placeholder='Name...'
+          onChange={(e) => changeName(e)}
+          ></input>
+          <button className='button'>submit</button>
           <input placeholder='Birthday...'></input>
-          <button>submit</button>
+          <button className='button'>submit</button>
           <input placeholder='Location...'></input>
-          <button>submit</button>
+          <button className='button'>submit</button>
         </form>
+      </div>
+      <div id='profile-card-container'>
+        <span>{name}</span>
       </div>
     </div>
   )
