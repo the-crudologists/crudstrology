8e16ee275ae22abc5c668dbdac1f9a29ea1c083b
colin <colin.williams91@gmail.com>
2023-01-23 17:35:31 -0600

    add passport and express-session, add auth file with google strategy, implement google strategy in routes inside server index, from login page user hits google api from server and stores profile in users table with user model from db, pages load and redirect correctly

--- server/auth.js
+++ server/auth.js
@@ -1,21 +1,48 @@
 const dotenv = require('dotenv').config();
+const passport = require('passport');
+const { User } = require('../database/index.js');
+const GoogleStrategy = require( 'passport-google-oauth2' ).Strategy;
 
 const { GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET, GOOGLE_API_KEY } = process.env;
 
-console.log(GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET);
+// console.log(GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET);
 
-const GoogleStrategy = require( 'passport-google-oauth2' ).Strategy;
+passport.use(new GoogleStrategy({
+    clientID: GOOGLE_CLIENT_ID,
+    clientSecret: GOOGLE_CLIENT_SECRET,
+    callbackURL: "http://localhost:8080/auth/google/callback",
+    passReqToCallback: true
+  },
+  (request, accessToken, refreshToken, profile, done) => {
+    console.log('GOOGLE PROFILE', profile); // pull profile.given_name
+    User.findOrCreate({ 
+      where: { 
+        googleId: profile.id,
+        name: profile.given_name 
+      }
+    })
+    .then((user, created) => {
+      console.log('Find Or Create Google Acct --> DB: Success', created, user);
+      done(null, user);
+    })
+    .catch(err => {
+      console.error('error FindOrCreate Google Auth', err);
+    })
+  }
+));
+
+// original callback passed to .findOrCreate({}, ...)
+// (err, user) => {
+//   return done(err, user);
+// });
 
 
-// passport.use(new GoogleStrategy({
-//     clientID:     GOOGLE_CLIENT_ID,
-//     clientSecret: GOOGLE_CLIENT_SECRET,
-//     callbackURL: "http://yourdomain:3000/auth/google/callback",
-//     passReqToCallback   : true
-//   },
-//   function(request, accessToken, refreshToken, profile, done) {
-//     User.findOrCreate({ googleId: profile.id }, (err, user) => {
-//       return done(err, user);
-//     });
-//   }
-// ));
+// ???
+passport.serializeUser((user, done) => {
+  done(null, user);
+});
+
+passport.deserializeUser((user, done) => {
+  done(null, user);
+});
+
--- server/index.js
+++ server/index.js
@@ -1,5 +1,12 @@
 const path = require('path');
 const express = require('express');
+const session = require('express-session');
+const passport = require('passport');
+
+const dotenv = require('dotenv').config();
+const { SERVER_SESSION_SECRET } = process.env;
+// try requiring files for database like this...
+require('./auth.js');
 const { seeder } = require('../database/index.js');
 //const db = require('../database/index.js');
 
@@ -12,6 +19,13 @@ const PORT = 8080;
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(express.static(DIST_DIR));
+app.use(session({secret: SERVER_SESSION_SECRET}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+const isLoggedIn = (req, res, next) => {
+  req.user ? next() : res.sendStatus(401);
+};
 
 // react home view*
 app.get('/', (req, res) => {
@@ -21,24 +35,40 @@ app.get('/', (req, res) => {
 // routes to google login page
 app.get('/login', (req, res) => {
   // href needs to be '/auth/google'
-  res.send('<a href="/">Authenticate with Google</a>');
+  res.send('<a href="/auth/google">Authenticate with Google</a>');
 });
 
-app.get('/auth/google', (req, res) => {
+// <-- PASSPORT DOCS
+app.get('/auth/google',
+  passport.authenticate('google', { scope:
+      [ 'email', 'profile' ] }
+));
 
-});
+// <-- working -->
+// app.get('/auth/google/callback',
+//   passport.authenticate('google', { failureRedirect: '/login' }), (req, res) => {
+//     res.redirect('/protected');
+//   }
+// );
+
+app.get('/auth/google/callback',
+  passport.authenticate('google', {
+      successRedirect: '/protected', // ex: '/auth/google/success'
+      failureRedirect: '/login' // ex: '/auth/google/failure'
+}));
+// <-- END PASSPORT DOCS
 
 // once user is logged in, route to 'logged-in' view*
-app.get('/protected', (req, res) => {
-  res.send('Hello');
+app.get('/protected', isLoggedIn, (req, res) => {
+  res.send('Log in success');
 });
 
 (async () => {
-
-  await seeder();
+  // <-- build seed script and call seeder() in that file...
+  // await seeder();
 
   app.listen(PORT, () => {
-    console.log(`listening on port: ${PORT}`)
+    console.log(`listening on port: http://localhost:${PORT}`)
   });
 
 })();
