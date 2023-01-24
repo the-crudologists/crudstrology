8393dcec342b529db30b911fd10201f82f88f90b
Benjamin Dickinson <bsadickinson@gmail.com>
2023-01-24 11:49:52 -0600

    added route and routes tags to App.jsx

--- client/App.jsx
+++ client/App.jsx
@@ -1,18 +1,23 @@
 import React from 'react';
 import NavBar from './NavBar.jsx';
-import {Route, Routes} from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 const App = () => (
-  <div>
-    <NavBar />
-    <a href="/auth/google">Authenticate with Google</a>
-    <Routes>
-      <Route path="/"></Route>
-      <Route path="/faves"></Route>
-      <Route path="/scopes"></Route>
-      <Route path="/reading"></Route>
-      <Route path="/dialog"></Route>
-    </Routes>
-  </div>
+  <>
+    <div>
+      <NavBar />
+      <a href="/auth/google">Authenticate with Google</a>
+
+    </div>
+    <div>
+      <Routes>
+        <Route path="/"></Route>
+        <Route path="/faves"></Route>
+        <Route path="/scopes"></Route>
+        <Route path="/reading"></Route>
+        <Route path="/dialog"></Route>
+      </Routes>
+    </div>
+  </>
 );
 
 export default App; 
