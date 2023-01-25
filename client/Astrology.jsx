c6ce412bb537cb13557418f0e84ef7a2c580042f
Benjamin Dickinson <bsadickinson@gmail.com>
2023-01-25 16:49:14 -0600

    add state to App/set state using the user db entry/pass state to Astrology component

--- client/Astrology.jsx
+++ client/Astrology.jsx
@@ -5,20 +5,21 @@ import { UserContext } from './App.jsx';
 const Astrology = () => {
 
   const [reading, setReading] = useState(fakeHoro);
-
+  const { dob, sign } = useContext(UserContext);
   return (
-    <UserContext.Consumer>
-      <div className='horoscope'>
-        <h1 className='horo-title'>Your Daily Horoscope</h1>
-        <div id='horo-item' className='container'>
-          {
-            Object.entries(reading).map((el, i) => {
-              return <div key={i}><b>{el[0]}</b>: <em>{el[1]}</em></div>;
-            })
-          }
-        </div>
+
+    <div className='horoscope'>
+      <h1 className='horo-title'>Your Daily Horoscope</h1>
+      <p>Your birthday is {dob}, so your sign is {sign}.</p>
+      <div id='horo-item' className='container'>
+        {
+          Object.entries(reading).map((el, i) => {
+            return <div key={i}><b>{el[0]}</b>: <em>{el[1]}</em></div>;
+          })
+        }
       </div>
-    </UserContext.Consumer>
+    </div>
+
   );
 };
 
--- server/index.js
+++ server/index.js
@@ -68,7 +68,9 @@ app.get('/auth/google/callback',
     if (!loggedInSessions[loggedInUser.googleId]) {
       loggedInSessions[loggedInUser.googleId] = {
         name: loggedInUser.name,
-        sessionID: req.sessionID
+        sessionID: req.sessionID,
+        dob: loggedInUser.dob,
+        sign: loggedInUser.sign
       };
     }
     console.log('Logged-In-Sessions OBJECT', loggedInSessions);
