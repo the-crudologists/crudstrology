ee66029cbb88380279ffea3ecf3a63ef7bae2d7e
Matthew <matthewjschiro@gmail.com>
2023-01-25 17:34:24 -0600

    setup server get for api, axios request on zenQuotes comp

--- client/Astrology.jsx
+++ client/Astrology.jsx
@@ -7,18 +7,18 @@ const Astrology = () => {
   const [reading, setReading] = useState(fakeHoro);
 
   return (
-    <UserContext.Consumer>
-      <div className='horoscope'>
-        <h1 className='horo-title'>Your Daily Horoscope</h1>
-        <div id='horo-item' className='container'>
-          {
-            Object.entries(reading).map((el, i) => {
-              return <div key={i}><b>{el[0]}</b>: <em>{el[1]}</em></div>;
-            })
-          }
-        </div>
+    // <UserContext.Consumer>
+    <div className='horoscope'>
+      <h1 className='horo-title'>Your Daily Horoscope</h1>
+      <div id='horo-item' className='container'>
+        {
+          Object.entries(reading).map((el, i) => {
+            return <div key={i}><b>{el[0]}</b>: <em>{el[1]}</em></div>;
+          })
+        }
       </div>
-    </UserContext.Consumer>
+    </div>
+    // </UserContext.Consumer>
   );
 };
 
