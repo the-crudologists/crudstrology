feecff8224a134e2aa0c607879654639c3c17e26
Miles Ager <miles.ager13@gmail.com>
2023-01-26 13:13:05 -0600

    add Styled.jsx

--- client/Styled.jsx
+++ client/Styled.jsx
@@ -1,41 +1,40 @@
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 
-const Title = styled.h1`
-  color: red;`;
-
-
-const Nav = styled.div`
+const NavStyle = styled.div`
   background-color: darkslategrey;
   display: flex;
   justify-content: space-between;
-  width: fit-content;
   flex-basis: auto;`;
 
 const NavUl = styled.div`
   font-size: 24px;
   padding: 10px;
   margin: 20px;
+  margin-right: 100px;
   display: flex;
   flex-basis: auto;
-  gap: 75px;
+  gap: 150px;
   text-shadow: 1px 1px 2px black, 0 0 1em black, 0 0 0.2em black;`;
 
 const NavUserInfo = styled.div`
   font-size: 20px;
-  padding-top: 66px;
+  padding-top: 62px;
+  padding-bottom: 62px;
   padding-left: 10px;
   padding-right: 10px;
+  margin-right: auto;
   text-shadow: 1px 1px 2px black, 0 0 1em black, 0 0 0.2em black;
   border: 1px solid azure;
-  margin-top: 5px;
-  margin-bottom: 5px;`;
+  margin-top: 10px;
+  margin-bottom: 10px;`;
 
 const NavImg = styled.img`
   object-fit: cover;
   border: 1px solid azure;
   background-color: black;
-  margin-top: 5px;
+  margin-top: 10px;
   margin-bottom: 5px;
   margin-left: 5px;`;
 
-export { Nav, Title, NavUl, NavUserInfo, NavImg };
+
+export { NavStyle, NavUl, NavUserInfo, NavImg };
