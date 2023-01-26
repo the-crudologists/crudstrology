8add507a041da16925cd599fcc1cd73ad17af01b
Matthew <matthewjschiro@gmail.com>
2023-01-26 13:35:25 -0600

    render quotes to feed, add button for quote, fixed bug with render

--- client/ZenQuotes.jsx
+++ client/ZenQuotes.jsx
@@ -1,36 +1,38 @@
 import React, { useState, useEffect } from 'react';
 //import fakeQuotes from '../database/fakeData/quotes.json';
 import axios from 'axios';
-
+import { BsHandThumbsUpFill } from 'react-icons/bs';
+import Button from 'react-bootstrap/Button';
 
 
 const ZenQuotes = () => {
   const [quotes, setQuote] = useState([]);
-  const getQuote = () => {
-    axios.get('/api/quotes')
-      .then(quote => {
-        setQuote(prevQuote => [quote.data, ...prevQuote]);
-      })
-      .catch(err => {
-        console.log('Axios Get /api/quotes', err);
-      });
-  };
-
   useEffect(() => {
-    setInterval(() => {
+    const getQuote = () => {
+      axios.get('/api/quotes')
+        .then(quote => {
+          setQuote(prevQuote => [quote.data, ...prevQuote]);
+        })
+        .catch(err => {
+          console.log('Axios Get /api/quotes', err);
+        });
+    };
+    const interval = setInterval(() => {
       getQuote();
-    }, 5000);
+    }, 10000);
+    return () => clearInterval(interval);
   }, []);
 
   return (
+
     <div>
       {quotes.map((quote, i) =>
-        <div key={i}>
+        <div key={i} className='quote'>
           <span>{quote.content}</span>
-          <span> -{quote.author}</span>
+          <span> -{quote.author}  </span>
+          <Button variant="primary"><BsHandThumbsUpFill /></Button>{' '}
         </div>
-      )
-      }
+      )}
     </div>);
 };
 
