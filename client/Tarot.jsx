1e5b0793739b7429028f00eae93db9ec2170ebd1
Miles Ager <miles.ager13@gmail.com>
2023-01-26 22:22:20 -0600

    create axios request for tarot

--- client/Tarot.jsx
+++ client/Tarot.jsx
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
 
 const Tarot = () => {
+
+  const [tarot, setTarot] = useState({});
+
+  useEffect(() => {
+    axios.get('/api/tarot')
+      .then(({ data }) =>
+        setTarot(data))
+      .catch((err) =>
+        console.log('ERROR in useEffect in Tarot.jsx: ', err));
+  });
+
+
   return (
     <div>
-      TAROT READING GOES HERE 
+      TAROT READING GOES HERE
     </div>
   );
 };
