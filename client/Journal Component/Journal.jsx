8e345ea9891d4547797ab322603d5cb9604b43ae
zacharydgomez <zachary.d.gomez@gmail.com>
2023-04-04 18:30:46 -0500

    added mockup data on journal component

--- client/Journal Component/Journal.jsx
+++ client/Journal Component/Journal.jsx
@@ -2,36 +2,22 @@ import React, { useState, useContext, useEffect } from 'react';
 import { useQuill } from 'react-quilljs';
 import 'quill/dist/quill.snow.css';
 import TextBox from './QuillText.jsx';
-const Journal = () =>{
-  // // variables for quill
-  // const { quill, quillRef } = useQuill();
-  // // setting state for entries in quill
-  // const [entries, setEntries] = useState([]);
-  // React.useEffect(() => {
-  //   if (quill) {
-  //     quill.clipboard.dangerouslyPasteHTML('<h1>Thoughts</h1>');
-  //   }
-  // }, [quill]);
- 
-  // const handleButtonClick = () => {
-  //   if (quillRef.current) {
-  //     // console.log(quillRef.current.querySelector('.ql-editor').innerText);
-  //     const newEntry = quillRef.current.querySelector('.ql-editor').innerText;
-  //     setEntries(prevEntries => [...prevEntries, newEntry]);
-      
-  //   }
-  // };
-  // // console.log(quill); // undefined > Quill Object
-  // // console.log(quillRef); // { current: undefined } > { current: Quill Editor Reference }
-
+import UserEntries from './UserEntries.jsx';
+import UserHoro from './UserHoro.jsx';
+const Journal = () => {
   return (
-    <div className='Journal' style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-      <h1 className='Journal-title'> Journal</h1>
-      <TextBox></TextBox>
-   
+    <div className='Journal' style={{ display: 'flex', justifyContent: 'center' }}>
+      <div className='left-column'>
+       
+        <UserEntries />
+      </div>
+      <div className='middle-column'>
+        <TextBox />
+      </div>
+      <div className='right-column'>
+        <UserHoro />
+      </div>
     </div>
-     
-  
   );
 };
 
