f14f865e31f56d1e19d80d4c4d0df8fcbf77e1a3
zacharydgomez <zachary.d.gomez@gmail.com>
2023-04-04 17:23:06 -0500

    refining journal

--- client/Journal Component/Journal.jsx
+++ client/Journal Component/Journal.jsx
@@ -1,25 +1,37 @@
 import React, { useState, useContext, useEffect } from 'react';
 import { useQuill } from 'react-quilljs';
 import 'quill/dist/quill.snow.css';
-import { AstroButton, UserHoro, OtherHoros } from '../Styled.jsx';
+
 const Journal = () =>{
+  // variables for quill
   const { quill, quillRef } = useQuill();
+  // setting state for entries in quill
+  const [entries, setEntries] = useState([]);
   React.useEffect(() => {
     if (quill) {
       quill.clipboard.dangerouslyPasteHTML('<h1>Thoughts</h1>');
     }
   }, [quill]);
-
-  console.log(quill); // undefined > Quill Object
-  console.log(quillRef); // { current: undefined } > { current: Quill Editor Reference }
+ 
+  const handleButtonClick = () => {
+    if (quillRef.current) {
+      // console.log(quillRef.current.querySelector('.ql-editor').innerText);
+      const newEntry = quillRef.current.querySelector('.ql-editor').innerText;
+      setEntries(prevEntries => [...prevEntries, newEntry]);
+      
+    }
+  };
+  // console.log(quill); // undefined > Quill Object
+  // console.log(quillRef); // { current: undefined } > { current: Quill Editor Reference }
 
   return (
     <div className='Journal'>
       <h1 className='Journal-title'> Journal</h1>
-    <div style={{ width: 500, height: 300 }}>
-      <div ref={quillRef} />
-        <button className='text' onClick = {(e)=> console.log("hi")}>Submit</button>
-    </div>
+      <div style={{ width: 500, height: 300 }}>
+        <div ref={quillRef} />
+        <button className='text' onClick={handleButtonClick}>Submit</button>
+      </div>
+      
     </div>
   );
 };
