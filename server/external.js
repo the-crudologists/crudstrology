e962ffbf9404a000dc28540f4eb691c04b6767f0
zacharydgomez <zachary.d.gomez@gmail.com>
2023-04-04 18:35:36 -0500

    linting

--- server/external.js
+++ server/external.js
@@ -26,18 +26,18 @@ External.get('/quotes', (req, res) => {
 // API
 External.post('/horo', (req, res) => {
   // console.log('____SERVER____');
-  console.log('REQ BODY', req.body)
+  console.log('REQ BODY', req.body);
   const { user } = req.body;
   // getting the sign to be in lowercase for new api
-  const {sign} = user
-  const lowercaseSign = sign.toLowerCase()
+  const {sign} = user;
+  const lowercaseSign = sign.toLowerCase();
   console.log('USER DESTRUCTURED', user);
-    axios.get(`http://sandipbgt.com/theastrologer/api/horoscope/${lowercaseSign}/today/`)
+  axios.get(`http://sandipbgt.com/theastrologer/api/horoscope/${lowercaseSign}/today/`)
   // axios.post(`https://aztro.sameerkumar.website?sign=${user.sign}&day=today`)
     .then(result => {
       console.log('RESULT from Aztro API', result.data);
-      const{horoscope, sunsign} = result.data;
-      const{mood, keywords, intensity} = result.data.meta;
+      const {horoscope, sunsign} = result.data;
+      const {mood, keywords, intensity} = result.data.meta;
       const newObj = {
         horoscope,
         sign: sunsign,
